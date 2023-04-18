816503f5e6fbc7f8645e2e412316298197535499
imvickykumar999 <imvickykumar999@gmail.com>
2023-04-18 09:51:45 +0530

    upload files

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
+from fileinput import filename
 from pathlib import Path
 import os
 
 app = Flask(__name__)
+app.jinja_env.globals.update(os=os)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':  
+        f = request.files['file']
+        saved = os.path.join('input', f.filename)
+        f.save(saved)  
+    
     new_path = sorted(Path('./input').iterdir(),
                         key=os.path.getmtime)
     
@@ -16,6 +24,7 @@ def home():
 @app.route('/input/<filename>')
 def send_vixtify(filename):
     return send_from_directory("input", filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True, 
