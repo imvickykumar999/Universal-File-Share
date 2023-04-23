f86fcf58fe525e433f1929dc67f6531f30a28baa
imvickykumar999 <imvickykumar999@gmail.com>
2023-04-23 11:32:05 +0530

    renamed

--- kivy APK/main.py
+++ kivy APK/main.py
@@ -6,32 +6,16 @@ from kivy.app import App
 from threading import Thread
 from kivy.uix.textinput import TextInput
 
-import logging, re
-import webbrowser
-
-logger = logging.getLogger('werkzeug')
-handler = logging.FileHandler('test.log')
-logger.addHandler(handler)
-
-
 class MyApp(App):
     def build(self):
         Thread(target=self.start_flask).start()
-        self.log_text = TextInput(multiline=True, text='Flask listening on ...\n')
+        self.log_text = TextInput(multiline=True, text='Flask is listening either\n')
 
-        with open('test.log', encoding='utf-8') as f:
-            save = f.read()
-
-        urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', save)[1] + ':5000'
-        self.log_text.insert_text(f'{urls}\n')
-
-        webbrowser.open(urls)
+        self.log_text.insert_text('http://192.168.0.104:5000 or\nhttp://192.168.0.102:5000')
         return self.log_text
-
 
     def start_flask(self):
         from vixsharefile import run_server
-
 
 if __name__ == '__main__':
     MyApp().run()
